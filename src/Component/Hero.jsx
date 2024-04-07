56de1fc17b5abbd4ab84a54c23b4aa148032538a
Rich-1007 <ghricheak@gmail.com>
2024-04-07 23:05:54 +0530

    updates

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,7 +1,72 @@
 import React from "react";
 
 function Hero() {
-  
+  const cardDetails = [
+    {
+      src: "src/assets/work-from-home.png",
+      main_text: "Work From Home",
+      job_num: "500 Active jobs",
+    },
+    {
+      src: "src/assets/Accountant-rafiki.png",
+      main_text: "Accountant",
+      job_num: "4909 Active Jobs",
+    },
+    {
+      src: "src/assets/Add to Cart-pana.png",
+      main_text: "BPO / Customer care",
+
+      job_num: "4332 Active Jobs",
+    },
+    {
+      src: "src/assets/New entries-amico.png",
+      main_text: "Data Entry / Back Office",
+      job_num: "333 Active Jobs",
+    },
+    {
+      src: "src/assets/Marketing-bro.png",
+
+      main_text: "Sales / Marketing",
+      job_num: "2335 Active Jobs",
+    },
+    {
+      src: "src/assets/New entries-amico.png",
+      main_text: "Receptionist / Front Office",
+      job_num: "7673 Active Jobs",
+    },
+    {
+      src: "src/assets/Hospital patient-cuate.png",
+      main_text: "Hospitality Executives",
+      job_num: "436 Active Jobs",
+    },
+    {
+      src: "src/assets/Low code development-bro.png",
+      main_text: "IT Software-Engineer",
+
+      job_num: "2345 Active Jobs",
+    },
+    {
+      src: "src/assets/Innovation-bro.png",
+      main_text: "Mechanic",
+      job_num: "987 Active Jobs",
+    },
+    {
+      src: "src/assets/Welcome aboard-bro.png",
+      main_text: "Receptionist / Front Office",
+      job_num: "567 Active Jobs",
+    },
+    {
+      src: "src/assets/Take Away-rafiki.png",
+      main_text: "Delivery",
+      job_num: "98 Active Jobs",
+    },
+    {
+      src: "src/assets/Chef-bro.png",
+      main_text: "Cook / Chef",
+      job_num: "378 Active Jobs",
+    },
+  ];
+
   return (
     <div className="flex  flex-wrap">
       {cardDetails.map((item) => (
